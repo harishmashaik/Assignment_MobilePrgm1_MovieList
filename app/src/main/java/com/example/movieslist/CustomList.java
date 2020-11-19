32bb83d225b6f0b789ab714154bebd94b13919f1
harishmashaik <harishmashaikk@gmail.com>
2020-11-19 03:23:59 -0500

    UI

---
+++ app/src/main/java/com/example/movieslist/CustomList.java
@@ -0,0 +1,14 @@
+package com.example.movieslist;
+
+public class CustomList {
+
+    private int mImgId;
+    private String mMovieName, mMovieRating, mMovieDescription;
+
+    public CustomList(int imgId,String MovieN,String movieR,String movieD)
+    {
+        mImgId = imgId;
+        mMovieName = MovieN;
+        mMovieRating = movieR;
+    }
+}
